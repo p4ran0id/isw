f1bc63d2acee2171e9fd922c5a03669fd9e6aa08
p4ran0id <dennis.davydov@gmail.com>
2015-10-14 22:29:39 +0200

    a2

---
+++ src/MyFirstMovieManager.java
@@ -0,0 +1,34 @@
+public class MyFirstMovieManager
+{
+	private Movie movie;
+	private Performer performer;
+
+	// constructor
+	public MyFirstMovieManager ()
+	{
+	}
+
+	public MyFirstMovieManager (Movie movie)
+	{
+		this.movie = movie;
+	}
+
+	public MyFirstMovieManager (Movie movie, Performer performer)
+	{
+		this.movie = movie;
+		this.performer = performer;
+	}
+
+	// add Performer to a Movie
+	public void addPerformer (Performer performer)
+	{
+		this.performer = performer;
+		performer.setMovie(this.movie);
+	}
+
+	// give all performer
+	public Performer findPerformer()
+	{
+		return performer;
+	}
+}
